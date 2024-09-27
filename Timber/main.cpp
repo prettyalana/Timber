310a7754166e83cf4ffdf04f3391d4f15646573f
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-26 22:06:45 -0500

    Main Timber game function - Include SFML library - Render a game window

---
+++ Timber/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Timber
+//
+//  Created by Alana Edwards on 9/2/24.
+//
+
+// Include important libraries here
+#include <SFML/Graphics.hpp>
+
+// Make code easier to type with "using namespace" using namespace sf;
+
+int main()
+// This is where the game starts from int main()
+{
+    // Create a video mode object VideoMode vm(1920, 1080);
+    
+    // Create and open a window for the game
+    sf::RenderWindow window(vm, "Timber!!!", style::Fullscreen);
+    
+    return 0;
+}
+
