faecad13f41121b7aeeeeea233f6bdc12cab56d6
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-26 22:51:42 -0500

    Add the main game loop

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -9,15 +9,42 @@
 #include <SFML/Graphics.hpp>
 
 // Make code easier to type with "using namespace" using namespace sf;
+using namespace sf;
 
 int main()
 // This is where the game starts from int main()
 {
-    // Create a video mode object VideoMode vm(1920, 1080);
+    // Create a video mode object
+    VideoMode vm(1920, 1080);
     
     // Create and open a window for the game
-    sf::RenderWindow window(vm, "Timber!!!", style::Fullscreen);
+    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+    
+    // Main game loop
+    while (window.isOpen())
+    {
+        
+        // Handle the player input
+        
+        
+        
+        
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
+                window.close();
+    }
+    
+    
+    // Update the scene
+
+    
+    // Draw the scene
+    
+    
+    // Clear everything from the last frame window.clear();
+    
+    // Draw our game scene here
+    
+    // Show everything we just drew window.display();
     
     return 0;
 }
-
