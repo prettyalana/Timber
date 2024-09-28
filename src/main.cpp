998be1a70560eda8c413e1de2718afb582a48d3b
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-28 18:33:22 -0500

    Remove CMAKE files

--- src/main.cpp
+++ src/main.cpp
@@ -16,35 +16,29 @@ int main()
 {
     // Create a video mode object
     VideoMode vm(1920, 1080);
-    
+
     // Create and open a window for the game
     RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
-    
+
     // Main game loop
     while (window.isOpen())
     {
-        
+
         // Handle the player input
-        
-        
-        
-        
-            if (Keyboard::isKeyPressed(Keyboard::Escape))
-                window.close();
+
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+            window.close();
     }
-    
-    
+
     // Update the scene
 
-    
     // Draw the scene
-    
-    
+
     // Clear everything from the last frame window.clear();
-    
+
     // Draw our game scene here
-    
+
     // Show everything we just drew window.display();
-    
+
     return 0;
 }
