92f72411bbb9d97fdd670a4c87f77e6ad0c0660a
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-28 21:56:43 -0500

    Render window in fullscreen mode and add event handler to main game loop

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -16,50 +16,51 @@ int main()
 // This is where the game starts from int main()
 {
     // Create a video mode object
-    VideoMode vm(800, 600); // Windowed mode
-
+    VideoMode vm(VideoMode::getDesktopMode()); // Fullscreen mode
+    
     // Create and open a window for the game
     RenderWindow window(vm, "Timber!!!");
-
+    
     // Create a texture to hold a graphic on the GPU
     Texture textureBackground;
-
-    // Load a graphic into the texture textureBackground.
-    if (!textureBackground.loadFromFile("graphics/background.png"))
-    {
+    
+    // Load a graphic into the texture
+    if (!textureBackground.loadFromFile("/Users/alana/alana-fullstackdev/Timber/graphics/background.png")) {
         std::cerr << "Failed to load image: graphics/background.png" << std::endl;
         return -1; // Exit if image loading fails
     }
-
+    
     // Create a sprite spriteBackground;
     Sprite spriteBackground;
-
+    
     // Attach the texture to the sprite spriteBackground.
     spriteBackground.setTexture(textureBackground);
-
+    
     // Set the spriteBackground to cover the screen sppriteBackground.
     spriteBackground.setPosition(0, 0);
-
+    
     // Main game loop
-    while (window.isOpen())
-    {
-
-        // Handle the player input
-
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
-            window.close();
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            // Handle the player input
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        
+        // Update the scene
+        
+        // Draw the scene
+        
+        // Clear everything from the last frame window.clear();
+        window.clear();
+        
+        // Draw our game scene here
+        window.draw(spriteBackground);
+        
+        // Show everything we just drew window.display();
+        window.display();
     }
-
-    // Update the scene
-
-    // Draw the scene
-
-    // Clear everything from the last frame window.clear();
-
-    // Draw our game scene here
-    window.draw(spriteBackground);
-
-    // Show everything we just drew window.display();
-
+    
     return 0;
 }
