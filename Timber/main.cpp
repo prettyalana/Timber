e843781b2eaa50792b2488155d12f8366b2d1ffa
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-28 21:01:46 -0500

    Add an error handler to render background - Updated cwd and program paths in launch.json

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -7,6 +7,7 @@
 
 // Include important libraries here
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 // Make code easier to type with "using namespace" using namespace sf;
 using namespace sf;
@@ -15,26 +16,30 @@ int main()
 // This is where the game starts from int main()
 {
     // Create a video mode object
-    VideoMode vm(1920, 1080);
+    VideoMode vm(800, 600); // Windowed mode
 
     // Create and open a window for the game
-    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
-    
+    RenderWindow window(vm, "Timber!!!");
+
     // Create a texture to hold a graphic on the GPU
     Texture textureBackground;
-    
+
     // Load a graphic into the texture textureBackground.
-    textureBackground.loadFromFile("graphics/background.png");
-    
+    if (!textureBackground.loadFromFile("graphics/background.png"))
+    {
+        std::cerr << "Failed to load image: graphics/background.png" << std::endl;
+        return -1; // Exit if image loading fails
+    }
+
     // Create a sprite spriteBackground;
     Sprite spriteBackground;
-    
+
     // Attach the texture to the sprite spriteBackground.
     spriteBackground.setTexture(textureBackground);
 
     // Set the spriteBackground to cover the screen sppriteBackground.
-    spriteBackground.setPosition(0,0);
-    
+    spriteBackground.setPosition(0, 0);
+
     // Main game loop
     while (window.isOpen())
     {
@@ -53,7 +58,7 @@ int main()
 
     // Draw our game scene here
     window.draw(spriteBackground);
-    
+
     // Show everything we just drew window.display();
 
     return 0;
