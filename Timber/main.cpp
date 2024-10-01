9b18f2a6fd33baf08b26349a24477c9c1ec7c6b7
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-09-30 20:37:10 -0500

    Animate clouds

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -155,6 +155,92 @@ int main()
             }
         }
 
+        // Manage the clouds
+        //Cloud 1
+        if (!cloud1Active)
+        {
+            // How fast is the cloud moving?
+            srand((int)time(0) * 10); // * 10 to the result returned by time(0) ensures we always get a different seed for each of the clouds
+            cloud1Speed = (rand() % 200);
+
+            // How high is the cloud?
+            srand((int)time(0) * 10);
+            float height = (rand() % 150);
+            spriteCloud1.setPosition(-200, height);
+            cloud1Active = true;
+        }
+        else 
+        {
+            // Move the cloud
+            spriteCloud1.setPosition(
+            spriteCloud1.getPosition().x +
+            (cloud1Speed * dt.asSeconds()),
+            spriteCloud1.getPosition().y);
+
+            // Has the cloud reached the right-hand edge of the screen?
+            if(spriteCloud1.getPosition().x > 1920)
+            {
+                cloud1Active = false;
+            }
+        }
+
+
+        //Cloud 2
+        if (!cloud2Active)
+        {
+            // How fast is the cloud moving?
+            srand((int)time(0) * 20); 
+            cloud2Speed = (rand() % 200);
+
+            // How high is the cloud?
+            srand((int)time(0) * 20);
+            float height = (rand() % 300) - 150;
+            spriteCloud2.setPosition(-200, height);
+            cloud2Active = true;
+        }
+        else 
+        {
+            // Move the cloud
+            spriteCloud2.setPosition(
+            spriteCloud2.getPosition().x +
+            (cloud2Speed * dt.asSeconds()),
+            spriteCloud2.getPosition().y);
+
+            // Has the cloud reached the left-hand edge of the screen?
+            if(spriteCloud2.getPosition().x > 1920)
+            {
+                cloud2Active = false;
+            }
+        }
+
+        //Cloud 3
+        if (!cloud3Active)
+        {
+            // How fast is the cloud moving?
+            srand((int)time(0) * 30); // * 10 to the result returned by time(0) ensures we always get a different seed for each of the clouds
+            cloud3Speed = (rand() % 200);
+
+            // How high is the cloud?
+            srand((int)time(0) * 30);
+            float height = (rand() % 450) - 150;
+            spriteCloud3.setPosition(-200, height);
+            cloud3Active = true;
+        }
+        else 
+        {
+            // Move the cloud
+            spriteCloud3.setPosition(
+            spriteCloud3.getPosition().x +
+            (cloud3Speed * dt.asSeconds()),
+            spriteCloud3.getPosition().y);
+
+            // Has the cloud reached the left-hand edge of the screen?
+            if(spriteCloud3.getPosition().x > 1920)
+            {
+                cloud3Active = false;
+            }
+        }
+
         // Draw the scene
 
         // Clear everything from the last frame window.clear();
